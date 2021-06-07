b36d895dfc2e8ece456564966c8be9d0a77d29f1
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-06-07 12:45:49 -0300

    adding secundary screen text image and title

--- app/src/main/java/com/example/loginui/InitialScreen.kt
+++ app/src/main/java/com/example/loginui/InitialScreen.kt
@@ -1,11 +1,22 @@
 package com.example.loginui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import com.example.loginui.databinding.ActivityInitialScreenBinding
+import com.example.loginui.databinding.ActivityMainBinding
 
 class InitialScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityInitialScreenBinding
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initial_screen)
+        binding = ActivityInitialScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
