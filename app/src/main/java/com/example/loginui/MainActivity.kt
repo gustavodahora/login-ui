6154a6e3d44aadde4ec94af78917fa5e44652916
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-06-07 11:42:17 -0300

    Adding a function to next screen on login

--- app/src/main/java/com/example/loginui/MainActivity.kt
+++ app/src/main/java/com/example/loginui/MainActivity.kt
@@ -1,15 +1,58 @@
 package com.example.loginui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.loginui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var checkedThemeGlobal = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.apply {
+            val checked = getBoolean("checked", false)
+            checkedThemeGlobal = checked
+        }
+
+        // Set them on startup
+        setTheme()
     }
+
+    fun theme(view: View?) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        var checked = binding.darkBtn.isChecked
+        editor.putBoolean("checked", checked).apply()
+        checkedThemeGlobal = checked
+
+        setTheme()
+    }
+
+    // Set theme
+    fun setTheme() {
+        binding.darkBtn.isChecked = checkedThemeGlobal
+        if (checkedThemeGlobal) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+
+    fun login(v: View) {
+        val intent = Intent(applicationContext, InitialScreen::class.java)
+        startActivity(intent)
+    }
+
+
 }
